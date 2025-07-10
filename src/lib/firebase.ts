159a49d2d9066581ed4ab32ae0ee9dd96d94f4c2
marlin andrews <marlinswd@gmail.com>
2025-07-10 07:10:38 +0000

    firestore.clients6.google.com/$rpc/google.firestore.admin.v1.FirestoreAd

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC5ucImm-HKI0Hs9hvzOzmMmn5w0XPyVWI",
+  authDomain: "we-be-live.firebaseapp.com",
+  projectId: "we-be-live",
+  storageBucket: "we-be-live.appspot.com",
+  messagingSenderId: "415342148606",
+  appId: "1:415342148606:web:3c33dbe9b6a3070fba5eaf",
 };
 
 // Initialize Firebase
